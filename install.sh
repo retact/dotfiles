e93c3ce0d33e1c88d9611ec8644f4e455a30e446
takutosoeda <003starrysky@gmail.com>
2021-07-31 04:13:56 +0900

    [create]install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+DIR=$(cd $(dirname $0);pwd)
+
+cd $DIR
+
+git submodule init
+git submodule update
+
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
+
+    ln -snfv "$DIR"/"$f" "$HOME"/"$f"
+done
