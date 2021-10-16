17627f0dbc7a7999524ecb1bdb1773c8bb936337
retact <49817230+retact@users.noreply.github.com>
2021-10-16 20:41:51 +0900

    [update] install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,42 @@
-DIR=$(cd $(dirname $0);pwd)
+#!/bin/bash
 
-cd $DIR
+DOT_DIR="$HOME/dotfiles"
 
-git submodule init
-git submodule update
+has() {
+    type "$1" > /dev/null 2>&1
+}
 
-for f in .??*
-do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".DS_Store" ] && continue
+if [ ! -d ${DOT_DIR} ]; then
+    if has "git"; then
+        git clone git@github.com:retact/dotfiles.git ${DOT_DIR}
+    elif has "curl" || has "wget"; then
+        TARBALL="https://github.com/retact/dotfiles/archive/main.tar.gz"
+        if has "curl"; then
+            curl -L ${TARBALL} -o main.tar.gz
+        else
+            wget ${TARBALL}
+        fi
+        tar -zxvf main.tar.gz
+        rm -f main.tar.gz
+        mv -f dotfiles-main "${DOT_DIR}"
+    else
+        echo "curl or wget or git required"
+        exit 1
+    fi
 
-    ln -snfv "$DIR"/"$f" "$HOME"/"$f"
-done
+    cd ${DOT_DIR}
+    for f in *;
+    do
+        [[ "$f" == ".git" ]] && continue
+        [[ "$f" == ".gitignore" ]] && continue
+        [[ "$f" == ".DS_Store" ]] && continue
+        [[ "$f" == "README.md" ]] && continue
+        [[ "$f" == "install.sh" ]] && continue
+
+        ln -snf $DOT_DIR/"$f" $HOME/".$f"
+        echo "Installed .$f"
+    done
+else
+    echo "dotfiles already exists"
+    exit 1
+fi
